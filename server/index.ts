fa99eba6e1cb604e09fef20394c689a8dc9a51dc
George Mills <georgemills28@hotmail.com>
2024-12-10 14:15:44 +1300

    Prepare for deployment

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,16 @@
 import server from './server.ts'
+import * as Path from 'node:path'
 
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
+
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(Path.resolve('public')))
+  server.use('/assets', express.static(Path.resolve('./dist/assets')))
+  server.get('*', (req, res) => {
+    res.sendFile(Path.resolve('./dist/index.html'))
+  })
+}
